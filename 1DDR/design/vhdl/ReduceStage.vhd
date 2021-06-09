7c049dbf479606fd988e8937c7176894c0620862
yyunon <yonsel.yuksel@gmail.com>
2021-06-09 22:13:39 +0200

    Ready for synth

--- 1DDR/design/vhdl/ReduceStage.vhd
+++ 1DDR/design/vhdl/ReduceStage.vhd
@@ -3,14 +3,14 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-library ieee_proposed;
-use ieee_proposed.fixed_pkg.all;
+--library ieee_proposed;
+--use ieee_proposed.fixed_pkg.all;
 
 library work;
 use work.ParallelPatterns_pkg.all;
 use work.Stream_pkg.all;
 use work.Tpch_pkg.all;
---use work.fixed_generic_pkg_mod.all;
+use work.fixed_generic_pkg_mod.all;
 
 entity ReduceStage is
   generic (
