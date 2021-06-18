e6d5c105be52e671767b6c346af603c8224d5a17
yyunon <yonsel.yuksel@gmail.com>
2021-06-18 15:01:47 +0200

    Change fixed_generic_pkg_mod pkg

--- 4DDR/design/vhdl/Filter.vhd
+++ 4DDR/design/vhdl/Filter.vhd
@@ -4,14 +4,14 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_misc.all;
 use ieee.numeric_std.all;
 
-library ieee_proposed;
-use ieee_proposed.fixed_pkg.all;
+--library ieee_proposed;
+--use ieee_proposed.fixed_pkg.all;
 
 library work;
 use work.Stream_pkg.all;
 use work.ParallelPatterns_pkg.all;
 use work.Tpch_pkg.all;
---use work.fixed_generic_pkg_mod.all;
+use work.fixed_generic_pkg_mod.all;
 
 -- In the first prototype generate different hws for each op.
 entity FILTER is
